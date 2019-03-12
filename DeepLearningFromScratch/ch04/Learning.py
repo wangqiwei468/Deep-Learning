58fd6334cdc710047f12e94f2b4585a5bdbbb792
Chivie <wangqiwei464@vip.qq.com>
2019-03-12 18:18:47 +0800

    update Learning.py

--- DeepLearningFromScratch/ch04/Learning.py
+++ DeepLearningFromScratch/ch04/Learning.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pylab as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+#%%
 #定义三维数据
 def function_2(x,y):
     return x**2 + y**2
-
+#%%
 fig = plt.figure()
 ax = Axes3D(fig)
 x = np.arange(-3,3,0.1)
@@ -17,3 +17,14 @@ plt.ylabel('y')
 
 ax.plot_surface(X,Y,Z,rstride=1,cstride=1,cmap='rainbow')
 plt.show()
+
+#%%
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / (2*h)
+
+#%%
+def function_temp(x0):
+    return x0**2 + 4.0**2.0
+
+x = numerical_diff(function_temp, 3.0)
