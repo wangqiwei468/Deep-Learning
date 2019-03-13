e2636a3f68757c1c0b8f591ded9195a35fe6914d
Chivie <wangqiwei464@vip.qq.com>
2019-03-13 13:47:28 +0800

    add gradient_simplenet.py

---
+++ DeepLearningFromScratch/ch04/gradient_simplenet.py
@@ -0,0 +1,41 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self,x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y,t)
+
+        return loss
+
+#%%
+net = simpleNet()
+print(net.W)    # 权重参数
+
+x = np.array([0.6, 0.9])
+p=net.predict(x)
+print(p)
+
+np.argmax(p)    # 最大值的索引
+
+t = np.array([1,0,0])
+net.loss(x,t)
+
+#%%
+# def f(W):
+#     return net.loss(x, t)
+f = lambda w: net.loss(x,t)
+dW = numerical_gradient(f, net.W)
+print(dW)
+
+
